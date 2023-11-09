50644303eecca5ecfe2eba19e6daedd479cd4742
rsahana30 <rangersahana30@gmail.com>
2023-11-09 09:36:57 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-
 import './App.css';
 
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Link
 } from "react-router-dom";
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        
         <Navbar/>
-        <News pageSize={6} country="in" category="Science"/>
+        <Route>
+        
+        <Switch>
+        <Route path="/"><News pageSize={6} country="in" category="General"/> </Route>
+        <Route path="/Business"><News pageSize={6} country="in" category="Business"/> </Route>
+        <Route path="/Entertainment"><News pageSize={6} country="in" category="Entertainment"/> </Route>
+        <Route path="/Health"><News pageSize={6} country="in" category="Health"/> </Route>
+        <Route path="/Science"><News pageSize={6} country="in" category="Science"/> </Route>
+        <Route path="/Sports"><News pageSize={6} country="in" category="Sports"/> </Route>
+        <Route path="/Technology"><News pageSize={6} country="in" category="Technology"/> </Route>  
        
-      </div>
+    </Switch>
+    
+   
+        </Route>
+    </div>
+     
     )
   }
 }
